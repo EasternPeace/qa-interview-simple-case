eefd32075734cea33f12f8952b41ade89f412006
Tsoi Anatoli <mamayatester@outlook.com>
2024-07-07 21:10:55 +0200

    refs Anatoli Tsoi added faker to generate user data

--- test-data/userData.ts
+++ test-data/userData.ts
@@ -1,20 +1,31 @@
+import { faker } from '@faker-js/faker';
+
 export const existingUsers = [
-    {
-      email: 'test1@mail.com',
-      password: 'testPassword!',
-      firstName: 'Test1',
-      lastName: 'Testsson1',
-    },
-    {
-      email: 'test2@mail.com',
-      password: 'testPassword!',
-      firstName: 'Test2',
-      lastName: 'Testsson2',
-    },
-    {
-      email: 'test3@mail.com',
-      password: 'testPassword!',
-      firstName: 'Test3',
-      lastName: 'Testsson3',
-    },
-  ] as const
+  {
+    email: 'test1@mail.com',
+    password: 'testPassword!',
+    firstName: 'Test1',
+    lastName: 'Testsson1',
+  },
+  {
+    email: 'test2@mail.com',
+    password: 'testPassword!',
+    firstName: 'Test2',
+    lastName: 'Testsson2',
+  },
+  {
+    email: 'test3@mail.com',
+    password: 'testPassword!',
+    firstName: 'Test3',
+    lastName: 'Testsson3',
+  },
+] as const
+
+export function generateUser() {
+  return {
+    email: faker.internet.email(),
+    password: faker.string.alphanumeric(9),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
+}
